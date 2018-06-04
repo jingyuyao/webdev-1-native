74ec0a4fef385641d8455a37bd5d17d2e8529972
Jingyu Yao <yjy.emails@gmail.com>
2018-06-04 13:37:58 -0400

    handle ios network code maybe

--- service/ServiceConfig.js
+++ service/ServiceConfig.js
@@ -1,6 +1,10 @@
+import {Platform} from 'react-native';
+
 // 10.0.2.2 is the default local machine loopback address for Android emulator.
 const hostname =
-  __DEV__ ? 'http://10.0.2.2:8080' : 'https://jingyuyao-webdev-1.herokuapp.com';
+  __DEV__
+    ? (Platform.OS === 'android' ? 'http://10.0.2.2:8080' : 'http://localhost:8080')
+    : 'https://jingyuyao-webdev-1.herokuapp.com';
 
 export const courseUrl = hostname + '/api/course';
 export const moduleUrl = hostname + '/api/module';
